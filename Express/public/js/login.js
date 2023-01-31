a7f3aed913f21c8840b2b36a8d5f34eb50f52a4b
Madhav <email>
2023-01-31 17:54:41 +1100

    ready to deploy

--- Express/public/js/login.js
+++ Express/public/js/login.js
@@ -6,7 +6,7 @@ export const login = async (email, password) => {
     //sending data using http request using a library called axios
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      url: '/api/v1/users/login',
       data: {
         email,
         password,
@@ -35,7 +35,7 @@ export const logout  = async ()=>{
   try{
     const res = await axios({
       method: 'GET',
-      url: 'http://127.0.0.1:3000/api/v1/users/logout',
+      url: '/api/v1/users/logout',
       
 
     })
